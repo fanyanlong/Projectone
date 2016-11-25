3b40e1d198c07f6aa6692aef90b21aeb4000b988
e <fanyanlong1991@163.com>
2016-11-25 15:19:37 +0800

    Listview
    测试提交listview源码

---
+++ ListViewDemo/app/src/main/java/com/bawei/listview/Data.java
@@ -0,0 +1,15 @@
+package com.bawei.listview;
+
+/**
+ * Created by yekh on 2016/11/1.
+ */
+
+public class Data {
+
+    public int index;
+
+    public String text;
+
+    public boolean isCheck;
+
+}
---
+++ ListViewDemo/app/src/main/java/com/bawei/listview/ScrollListView.java
@@ -0,0 +1,30 @@
+package com.bawei.listview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by yekh on 2016/11/2.
+ */
+//事件分发机制
+public class ScrollListView extends ListView{
+    public ScrollListView(Context context) {
+        super(context);
+    }
+
+    public ScrollListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
